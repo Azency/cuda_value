dac747b0c9344fcef90e8dd678746f486208c4e6
hhf1357924680 <13592590769@163.com>
2025-07-19 01:11:37 +0800

    A100-main_update

--- cudaC/testmain.sh
+++ cudaC/testmain.sh
@@ -4,6 +4,6 @@ nvcc -lineinfo -g -O3 -Xcompiler -Wall -dc src/cuda_value.cu -o cuda_value.o
 nvcc -lineinfo -g -O3 -Xcompiler -Wall -dc src/testmain.cu -o testmain.o
 nvcc cuda_value.o testmain.o -o ./testmain -lcudadevrt
 rm -rf cuda_value.o testmain.o
-CUDA_VISIBLE_DEVICES=4 ./testmain
+CUDA_VISIBLE_DEVICES=0 ./testmain
 rm -rf ./testmain
 
